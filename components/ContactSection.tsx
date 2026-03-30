94478de4bb9259a53768ba77dfe28e93f490f1fa
Team-Opelion-Global <opelionglobal@gmail.com>
2026-03-30 18:55:43 +0530

    Update ContactSection.tsx

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -78,7 +78,7 @@ export default function ContactSection() {
             </p>
 
             <p className="text-md text-[#1f3c5b] font-medium">
-              <span className="grid" style={{ gridTemplateColumns: "auto 1fr", gap: "0 12px" }}>
+              <span className="grid" style={{ gridTemplateColumns: "auto 1fr", gap: "0 8px" }}>
               <span>Monday-Friday</span><span>: 9:00 AM - 6:00 PM</span>
               <span>Saturday</span><span>: 9:00 AM - 4:00 PM</span>
               <span>Sunday</span><span>: Closed</span>
