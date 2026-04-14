d6685c5024c88f30ba54c72b6edd4b7a25caa61f
Team-Opelion-Global <opelionglobal@gmail.com>
2026-04-14 16:19:58 +0530

    Update favicon and metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,19 @@ const inter = Inter({
 export const metadata = {
   title: "Welcome to Opelion Global Private Limited",
   description: "Opelion Global Private Limited specializes in the end-to-end sourcing, processing, quality assurance, and export of premium agro products to international markets.",
+  // icons: {
+  //   icon: "/LOGO_Opelion.png",
+  //   shortcut: "/LOGO_Opelion.png",
+  //   apple: "/LOGO_Opelion.png",
+  // },
   icons: {
-    icon: "/Logo_shadow.webp",
-    shortcut: "/Logo_shadow.webp",
-    apple: "/Logo_shadow.webp",
-  },
+  icon: [
+    { url: "/favicon.ico" , sizes:"any" },
+    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+  ],
+  apple: "/favicon-48x48.png",
+}
 };
 
 export default function RootLayout({
@@ -32,9 +40,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${inter.variable}`}>
       <head>
-        <link rel="icon" type="image/webp" href="/Logo_shadow.webp" />
+        {/* <link rel="icon" type="image/webp" href="/Logo_shadow.webp" />
         <link rel="shortcut icon" href="/Logo_shadow.webp" />
-        <link rel="apple-touch-icon" href="/Logo_shadow.webp" />
+        <link rel="apple-touch-icon" href="/Logo_shadow.webp" /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
